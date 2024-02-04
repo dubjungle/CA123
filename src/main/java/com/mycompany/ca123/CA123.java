ebf0bb8f2ecc92c2a09bbe1c18f8ccef3432227e
dubjungle <146137369+dubjungle@users.noreply.github.com>
2024-02-04 14:56:11 +0000

    comments

--- src/main/java/com/mycompany/ca123/CA123.java
+++ src/main/java/com/mycompany/ca123/CA123.java
@@ -20,47 +20,64 @@ public class CA123 {
     /**
      * @param args the command line arguments
      */
+    
+    
     public static void main(String args[]){
-        try {
+        try { //  FileReader reading text from the students.txt file
+              // FileWriter writing data wich one is correct. For this project.  
             Scanner sc = new Scanner(new FileReader("Students.txt"));
              FileWriter statusFile = new FileWriter("stats.txt");
              
-             
+            
              
             String workload = null;
             
+            // This variable stores the firstName of students.
             String firstName;
-
+            // This name variable stores the lastName of students.
             String lastName;
-            
+            //This variable holds the number of classes a student is enrolling in
             int numberOfClasses;
-            
+            //This variable stores the student number.
             String studentNumber;
             
+            // This line declares and initiliazes an array string wich is named eachLine and size of it is 3.
             String[] eachLine = new String[3];
             
+            // this loop continuos as long as there another line available.
             for (int i = 1; sc.hasNextLine(); i++) {
-               
+                
+                
+               // This loop iterates three times (j starts from 0 and goes up to 2).
                 for (int j = 0; j <= 2; j++) {
                 eachLine[j] = sc.nextLine();
-           
+                
+                // check or j it's equals to 2.
                     if(j==2){  
-                    
+                        
+                    // Split the first line of input (eachLine[0]) using space as the delimiter.
                     String[] splitNames = eachLine[0].split(" ");
-                    
+                    // Extracting the first and Last names from the splitNames array.
                     firstName = splitNames[0];
                     lastName = splitNames[1];
+                    // Parse the second line of eachLine[1] as a integer to get number of classes
                    numberOfClasses = Integer.parseInt(eachLine[1]);
+                   //this line is directly assigned to the studentNumber.
                    studentNumber = eachLine[2];
                    
                     
+                    // Print the full name
+                    // Print the numbers of classes stored in "numberOfClasses" variable.
+                    // Print the student numbers stored in "studentNumber" variable.
                     
 
                     System.out.println( firstName + " " + lastName);
                         System.out.println(numberOfClasses);
                        System.out.println(studentNumber);
                        
-                       
+                       //Switch statment based on the numberOfClasses variable
+                      // It assigns a workload category to the 'workload' variable based on the number of classes.
+
                        
                        switch (numberOfClasses) {
                             case 1:
@@ -88,9 +105,13 @@ public class CA123 {
                                 workload = "Full Time";
                                 
                         }
+                     // Check if the 'firstName' contains only letters using a regular expression.
+                     // The regular expression [a-zA-Z]+ ensures that the string consists of one or more letters.
                      
                         if (!firstName.matches("[a-zA-Z]+")) {
-                System.out.println("Error: first name must be only letters  ");
+                System.out.println("Error: first name must be only letters  "
+                );     // If the condition is true print an error message indicating that the first name
+
             }
             else if (lastName.matches("^[A-Za-z]+ [A-Za-z0-9]+$")) {
                 System.out.println ("No symbols please ");
