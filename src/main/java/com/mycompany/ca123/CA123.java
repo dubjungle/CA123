18dced01ac6c5cf3713e9348f3176828d8ed1d8e
dubjungle <146137369+dubjungle@users.noreply.github.com>
2024-02-04 15:45:39 +0000

    changes

--- src/main/java/com/mycompany/ca123/CA123.java
+++ src/main/java/com/mycompany/ca123/CA123.java
@@ -28,94 +28,117 @@ public class CA123 {
             Scanner sc = new Scanner(new FileReader("Students.txt"));
              FileWriter statusFile = new FileWriter("stats.txt");
              
-            
+            // This means it currently doesn't point to any specific string or object
              
+
             String workload = null;
+            // this variable stores the firstName of students
             
-            // This variable stores the firstName of students.
             String firstName;
-            // This name variable stores the lastName of students.
+            // this name variable stores the lastName of students
+            
             String lastName;
+            
             //This variable holds the number of classes a student is enrolling in
             int numberOfClasses;
-            //This variable stores the student number.
-            String studentNumber;
+            //this variable stores the student number.
             
+            String studentNumber;
             // This line declares and initiliazes an array string wich is named eachLine and size of it is 3.
+            
             String[] eachLine = new String[3];
+            
             
             // this loop continuos as long as there another line available.
             for (int i = 1; sc.hasNextLine(); i++) {
                 
                 
-               // This loop iterates three times (j starts from 0 and goes up to 2).
-                for (int j = 0; j <= 2; j++) {
-                eachLine[j] = sc.nextLine();
+               
+
+               // this loop iterates three times (j starts from 0 and goes up to 2 )
+               
+                    for (int j = 0; j <=2; j++)
+                 {eachLine[j] = sc.nextLine();
+                
                 
                 // check or j it's equals to 2.
                     if(j==2){  
                         
                     // Split the first line of input (eachLine[0]) using space as the delimiter.
                     String[] splitNames = eachLine[0].split(" ");
-                    // Extracting the first and Last names from the splitNames array.
+                  
+                    //Extracting the first and last name from the splitNames array.
                     firstName = splitNames[0];
                     lastName = splitNames[1];
-                    // Parse the second line of eachLine[1] as a integer to get number of classes
+                    
+                    //Parse the second line of eachLine[1] as a integer to get number of classes
                    numberOfClasses = Integer.parseInt(eachLine[1]);
+                   
                    //this line is directly assigned to the studentNumber.
                    studentNumber = eachLine[2];
                    
                     
-                    // Print the full name
-                    // Print the numbers of classes stored in "numberOfClasses" variable.
-                    // Print the student numbers stored in "studentNumber" variable.
+      
+                   
                     
 
                     System.out.println( firstName + " " + lastName);
                         System.out.println(numberOfClasses);
                        System.out.println(studentNumber);
+                    // Print the full name
+                    //Print the number of classes stored in numberOfClasses variable
+                    // Print the student number stored in studentNUmber variable
                        
                        //Switch statment based on the numberOfClasses variable
                       // It assigns a workload category to the 'workload' variable based on the number of classes.
 
                        
-                       switch (numberOfClasses) {
-                            case 1:
-                                workload = "Very Light";
-                                break;
-                            case 2:
-                                workload = "Light";
-                                break;
-                            case 3:
-                                workload = "Part Time";
-                                break;
-                            case 4:
-                                workload = "Part Time";
-                                break;
-                            case 5:
-                                workload = "Part Time";
-                                break;
-                            case 6:
-                                workload = "Full Time";
-                                break;
-                            case 7:
-                                workload = "Full Time";
-                                break;
-                            case 8:
-                                workload = "Full Time";
+                      
+                      switch (numberOfClasses){
+                          case 1:
+                              workload = "Very Light";
+                              break;
+                          case 2:
+                              workload = "Light";
+                              break;
+                          case 3: 
+                              workload = "Part Time";
+                              break;
+                          case 4: 
+                              workload = "Part Time";
+                              break;
+                          case 5: 
+                              workload = "Part Time";
+                              break;
+                          case 6:
+                              workload = "Full time";
+                              break;
+                          case 7:
+                              workload ="Full time";
+                              break;
+                          case 8:
+                              workload = "Full Time";
+                              break;
                                 
                         }
-                     // Check if the 'firstName' contains only letters using a regular expression.
-                     // The regular expression [a-zA-Z]+ ensures that the string consists of one or more letters.
                      
-                        if (!firstName.matches("[a-zA-Z]+")) {
+                     // Check if the "firstName" contains only letters using a regular expression.
+                     
+                     
+                     // the regular expression [a-zA-Z]+ ensures that the string consists of on or more letters
+                     
+                        
+                        if (!firstName.matches("a-zA-Z+")){
                 System.out.println("Error: first name must be only letters  "
                 );     // If the condition is true print an error message indicating that the first name
 
             }
-            else if (lastName.matches("^[A-Za-z]+ [A-Za-z0-9]+$")) {
+
+
+                        
+                        else if(lastName.matches("[A-Za-z]+ [A-Za-z0-9]+$")){
                 System.out.println ("No symbols please ");
-                 // The regular expression "^[A-Za-z]+ [A-Za-z0-9]+$" expects the last name to:
+                 // The regular expression "^[A-Za-z]+ [A-Za-z0-9]+$" expects to lastName to:
                  // Start with one or more letters.
                  // Followed by a space.
                  // End with one or more letters or numbers.
@@ -137,8 +160,7 @@ public class CA123 {
               else if (!studentNumber.matches("^\\d{2}[A-Za-z]{2,5}\\d+$")) {
                   
               System.out.println("Error: The first two characters must be numbers.");
-              // Check if the 'studentNumber' does not match a specific pattern using a regular expression.
-              // The regular expression "^\\d{2}[A-Za-z]{2,5}\\d+$" enforces the following pattern:
+              
               // - Starts with two digits.
               // - Followed by 2 to 5 uppercase or lowercase letters.
               // - Ends with one or more digits.
